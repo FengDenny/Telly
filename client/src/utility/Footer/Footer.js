a1e4e45537b82ab7879f86ab7016a6547d580388
FengDenny <fengdennyy@gmail.com>
2021-02-21 17:10:24 -0800

    Create Footer.js

---
+++ client/src/utility/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className='footer primary-bg'>
+      <div className='container '>
+        <header className='footer-header '>
+          <h1 className='white-color font-sm'>
+            © 2021 Telly, DF. All rights reserved.
+          </h1>
+        </header>
+      </div>
+    </div>
+  );
+}
+export default Footer;
