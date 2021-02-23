36a28a6691ac989ba6e096046b9c17dcb5365a00
FengDenny <fengdennyy@gmail.com>
2021-02-23 15:51:11 -0800

    Create alerts.js

---
+++ client/src/js/alerts.js
@@ -0,0 +1,15 @@
+export const hideAlert = () => {
+  // select .alert class and remove it
+  const el = document.querySelector(".alert");
+  if (el) el.parentElement.removeChild(el);
+};
+// if type is success or error
+export const showAlert = (type, message) => {
+  hideAlert();
+  const markup = `<div class=" alert alert--${type}">${message}</div>`;
+
+  document.getElementById("form").insertAdjacentHTML("afterbegin", markup);
+
+  //   hide the alert after 5sec
+  window.setTimeout(hideAlert, 5000);
+};
