927b1d4ab89d04ec90029bf04fcf8e1f03a85e72
FengDenny <fengdennyy@gmail.com>
2021-02-22 16:40:25 -0800

    Error handler initialized

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,24 +1,29 @@
 const User = require("../models/User");
+const CatchAsync = require("../utility/CatchAsync");
+const AppError = require("../utility/AppError");
 
-exports.register = async (req, res) => {
+exports.register = CatchAsync(async (req, res, next) => {
   const { name, email, password } = req.body;
   // validations
-  if (!name) return res.status(400).send("Name is required");
-  if (!password || password.length < 6)
-    return res
-      .status(400)
-      .send("Password is required and must be at least 6 characters or more");
   let user = await User.findOne({ email }).exec();
-  if (user) return res.status(400).send("Email is taken");
+  if (user) {
+    return next(
+      new AppError(`${email} is already registered. Please login`, 401)
+    );
+  }
   // register user
   const newUser = new User(req.body);
 
   try {
     await newUser.save();
     console.log("USER CREATED", newUser);
-    return res.json({ status: "success", newUser });
+    return res.json({
+      status: "success",
+      message: `${email} has signed up successfully! Please login. `,
+      user: newUser,
+    });
   } catch (err) {
     console.log("REGISTERED FAIL", err);
     return res.status(400).send("Error");
   }
-};
+});
