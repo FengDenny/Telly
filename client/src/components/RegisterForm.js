8c7d654035e57583af4e7756b5eec434657a5a9a
FengDenny <fengdennyy@gmail.com>
2021-02-22 16:37:43 -0800

    Switch input/label order
    Also added placeholder and needed to put input first before the label to use :placeholder-shown css

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -11,55 +11,71 @@ const RegisterForm = ({
   <div className='form-section flex-center margin-center'>
     <form className='card ' onSubmit={handleSubmit}>
       <div className='form-group flex-direction-column'>
+        <input
+          id='name'
+          type='text'
+          className='form-input'
+          name='name'
+          placeholder='name'
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          required
+        />
         <label
           htmlFor='name'
           className=' secondary-heading secondary-color font-l'
         >
           Name
         </label>
-        <input
-          id='name'
-          type='text'
-          className='form-input'
-          name='name'
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
+        <div className='validation'>Name is required</div>
       </div>
       <div className='form-group flex-direction-column'>
+        <input
+          id='email'
+          type='email'
+          className='form-input'
+          name='email'
+          placeholder='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+
         <label
           htmlFor='email'
           className=' secondary-heading secondary-color font-l'
         >
           Email
         </label>
-        <input
-          id='email'
-          type='text'
-          className='form-input'
-          name='email'
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
+        <div className='validation'>Must be a valid email</div>
       </div>
+
       <div className='form-group flex-direction-column'>
+        <input
+          id='password'
+          type='password'
+          className='form-input'
+          name='password'
+          placeholder='password'
+          pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
         <label
           htmlFor='password'
           className=' secondary-heading secondary-color font-l'
         >
           Password
         </label>
-        <input
-          id='password'
-          type='password'
-          className='form-input'
-          name='password'
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
+        <div className='validation'>
+          Password must be at least 6 characters long, containing one uppercase,
+          lowercase, and number.
+        </div>
       </div>
+
       <div className='form-group flex-direction-column'>
-        <p className='secondary-heading secondary-color '>
+        <p className='signup-agreement secondary-heading secondary-color '>
           By Joining, you agree to our {""}
           <Link to='#' className='signup-agreement-links '>
             Terms
