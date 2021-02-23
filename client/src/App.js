a4d78c5e31ee46de9532eca3c4cb8683d8be9bd8
FengDenny <fengdennyy@gmail.com>
2021-02-23 14:59:47 -0800

    Added page titles

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,54 @@
 import "./css/App.css";
 import "./css/index.css";
 import "./css/MediaQueries.css";
+import { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./auth/Login";
-import Join from "./auth/Join";
-import Home from "./booking/Home";
+import Login from "./pages/auth/Login";
+import Join from "./pages/auth/Join";
+import Home from "./pages/cover/Home";
 import Navbar from "./utility/Navbar/Navbar";
 import Footer from "./utility/Footer/Footer";
+import Page from "./pages/components/pageTitle";
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <div className='App-content'>
-          <Route path='/' component={Navbar} />
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/join' component={Join} />
+            <Route
+              exact
+              path='/'
+              render={(props) => (
+                <Page title='Telly | Join for free or Login '>
+                  <Home {...props} />
+                </Page>
+              )}
+            />
+            <Fragment>
+              <Navbar />
+              <Route
+                exact
+                path='/login'
+                render={(props) => (
+                  <Page title='Login to Telly'>
+                    <Login {...props} />
+                  </Page>
+                )}
+              />
+              <Route
+                exact
+                path='/join'
+                render={(props) => (
+                  <Page title='Join Telly for free'>
+                    <Join {...props} />
+                  </Page>
+                )}
+              />
+            </Fragment>
           </Switch>
         </div>
-        <Route path='/' component={Footer} />
+        <Footer />
       </div>
     </Router>
   );
