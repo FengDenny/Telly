7883ae5d4b6b3256fc8626f3a37c6f25b2b630e4
FengDenny <fengdennyy@gmail.com>
2021-02-23 18:49:27 -0800

    login route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { register } = require("../controllers/authControllers");
+const { register, login } = require("../controllers/authControllers");
 
 const router = express.Router();
 
 router.post("/register", register);
+
+router.post("/login", login);
 
 module.exports = router;
