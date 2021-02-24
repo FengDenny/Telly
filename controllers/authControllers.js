69d2c01f464fea57d9625653b9312c3ac9cfef81
FengDenny <fengdennyy@gmail.com>
2021-02-23 18:48:45 -0800

    login auth

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const CatchAsync = require("../utility/CatchAsync");
 const AppError = require("../utility/AppError");
+const jwt = require("jsonwebtoken");
 
 exports.register = CatchAsync(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -23,5 +24,44 @@ exports.register = CatchAsync(async (req, res, next) => {
   } catch (err) {
     console.log("REGISTERED FAIL", err);
     return res.status(400).send("Error");
+  }
+});
+
+exports.login = CatchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email }).select("-__v");
+
+    if (!user) {
+      return next(new AppError("Email or password is invalid.", 401));
+    }
+    // compare password
+    user.comparePassword(password, (err, match) => {
+      console.log("COMPARE PASSWORD IN LOGIN ERROR", err);
+      if (!match || err) {
+        return next(new AppError("Email or password is invalid.", 401));
+      }
+      // GENERATE JWT TOKEN
+      let token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
+
+      const { _id, name, email, createdAt, updatedAt } = user;
+      return res.json({
+        status: "success",
+        message: `Welcome back, ${name}`,
+        token,
+        user: {
+          _id,
+          name,
+          email,
+          createdAt,
+          updatedAt,
+        },
+      });
+    });
+  } catch (err) {
+    console.log("LOGIN ERROR", err);
+    return res.status(400).send("Login failed");
   }
 });
