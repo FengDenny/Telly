0aee0b5770292ab620cb0e5c3ac5a4677c354e1f
FengDenny <fengdennyy@gmail.com>
2021-02-23 17:39:46 -0800

    fixed undefined error

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-export const register = async (user) => {
+export const register = async (user) =>
   await axios.post("/api/v1/register", user);
-};
+
+export const login = async (user) => await axios.post("/api/v1/login", user);
