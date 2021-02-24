87e0e0a393772f98563bed07acecd0c124b76036
FengDenny <fengdennyy@gmail.com>
2021-02-23 18:46:08 -0800

    redux useDispatch

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
-import { register } from "../../actions/authAction";
+import { login } from "../../actions/authAction";
 import { showAlert } from "../../js/alerts";
 import LoginForm from "./../components/loginForm";
+import { useDispatch } from "react-redux";
 function Login({ history }) {
-  const [email, setEmail] = useState("dfeezy4150@gmail.com");
+  const [email, setEmail] = useState("Dfeng415@yahoo.com");
   const [password, setPassword] = useState("415aAa");
   const [error, setError] = useState(null);
 
+  const dispatch = useDispatch();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log("LOGGED IN DATA:", { email, password });
     const data = {
       email,
       password,
     };
     try {
-      const res = await register(data);
-      console.log("REGISTER USER:", res);
-      history.push("/login");
+      const res = await login(data);
+      console.log(res);
+      if (res.data) {
+        console.log("SAVE USER RES IN REDUX AND LOCAL STORAGE THEN REDIRECT: ");
+        console.log(res.data);
+        // save user and token to localstorage
+        window.localStorage.setItem("auth", JSON.stringify(res.data));
+        // save user and token to redux
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: res.data,
+        });
+        history.push("/user_dashboard");
+      }
     } catch (err) {
       console.log(err);
       setError(showAlert("error", err.response.data.message));
