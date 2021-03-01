fed73cd4a6d388292fe9c0d7cf6854d35f7d20f9
FengDenny <fengdennyy@gmail.com>
2021-02-28 18:54:39 -0800

    Private user routing

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,18 @@ import "./css/index.css";
 import "./css/MediaQueries.css";
 import { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./pages/components/PrivateRoute";
+// components
 import Login from "./pages/auth/Login";
 import Join from "./pages/auth/Join";
 import Home from "./pages/cover/Home";
 import Navbar from "./utility/Navbar/Navbar";
 import Footer from "./utility/Footer/Footer";
 import Page from "./pages/components/pageTitle";
+import UserDashboard from "./pages/user/UserDashboard";
+import UserSellerDashboard from "./pages/user/UserSellerDashboard";
+import HotelBooking from "./pages/booking/HotelBooking";
+import AddHotel from "./pages/hotel/NewHotels";
 
 function App() {
   return (
@@ -42,6 +48,42 @@ function App() {
                 render={(props) => (
                   <Page title='Join Telly for free'>
                     <Join {...props} />
+                  </Page>
+                )}
+              />
+              <Route
+                exact
+                path='/hotel_booking'
+                render={(props) => (
+                  <Page title='Telly | Book your hotel'>
+                    <HotelBooking {...props} />
+                  </Page>
+                )}
+              />
+              <Route
+                exact
+                path='/hotels/new'
+                render={(props) => (
+                  <Page title='Telly | Add new hotel'>
+                    <AddHotel {...props} />
+                  </Page>
+                )}
+              />
+              <PrivateRoute
+                exact
+                path='/user_dashboard'
+                render={(props) => (
+                  <Page title='Users dashboard | Telly'>
+                    <UserDashboard {...props} />
+                  </Page>
+                )}
+              />
+              <PrivateRoute
+                exact
+                path='/user_dashboard/seller'
+                render={(props) => (
+                  <Page title='Users seller dashboard | Telly'>
+                    <UserSellerDashboard {...props} />
                   </Page>
                 )}
               />
