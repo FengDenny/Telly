7aa382c597daa3fa5977c33fe71c144ccc5ff2f4
FengDenny <fengdennyy@gmail.com>
2021-03-01 17:06:18 -0800

    Stripe account/payment status

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -31,3 +31,62 @@ exports.createConnectStripe = CatchAsync(async (req, res, next) => {
   console.log(link);
   res.send(link);
 });
+
+const updateStripePaymentDays = async (accountId) => {
+  const account = await stripe.accounts.update(accountId, {
+    settings: {
+      payouts: {
+        schedule: {
+          delay_days: 7,
+        },
+      },
+    },
+  });
+  return account;
+};
+
+exports.getAccountStatus = CatchAsync(async (req, res, next) => {
+  // console.log("GET ACCOUNT STATUS");
+  const user = await User.findById(req.user._id).exec();
+  const account = await stripe.accounts.retrieve(user.stripe_account_id);
+  // console.log("User Account Retrieve", account);
+  // update delay days
+  const updatedAccount = await updateStripePaymentDays(account.id);
+  const updatedUser = await User.findByIdAndUpdate(
+    user._id,
+    { stripe_seller: updatedAccount },
+    { new: true }
+  )
+    .select("-password")
+    .exec();
+
+  res.json(updatedUser);
+});
+
+exports.getAccountBalance = CatchAsync(async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+    const balance = await stripe.balance.retrieve({
+      stripeAccount: user.stripe_account_id,
+    });
+    res.json(balance);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+exports.payoutSetting = CatchAsync(async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).exec();
+    const loginLink = await stripe.accounts.createLoginLink(
+      user.stripe_account_id,
+      {
+        redirect_url: process.env.STRIPE_SETTING_REDIRECT_URL,
+      }
+    );
+    console.log("LOGIN LINK", loginLink);
+    res.json(loginLink);
+  } catch (err) {
+    return next(new AppError(err, 400));
+  }
+});
