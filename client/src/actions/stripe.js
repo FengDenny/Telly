b43dd28ac4980322a0437c4d00b0a38ad54368c7
FengDenny <fengdennyy@gmail.com>
2021-03-01 16:59:01 -0800

    Check users Stripe account status/balance

--- client/src/actions/stripe.js
+++ client/src/actions/stripe.js
@@ -10,3 +10,43 @@ export const createConnectAccount = async (token) =>
       },
     }
   );
+
+export const getAccountStatus = async (token) =>
+  axios.post(
+    "/api/v1/get-account-status",
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token} `,
+      },
+    }
+  );
+
+export const getAccountBalance = async (token) =>
+  axios.post(
+    "/api/v1/get-account-balance",
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token} `,
+      },
+    }
+  );
+
+export const payoutSetting = async (token) =>
+  await axios.post(
+    "/payout-setting",
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token} `,
+      },
+    }
+  );
+
+export const currencyFormat = (data) => {
+  return (data.amount / 100).toLocaleString(data.currency, {
+    style: "currency",
+    currency: data.currency,
+  });
+};
