02fc2085db05fdf73355643b92838c32ef872cd6
FengDenny <fengdennyy@gmail.com>
2021-03-01 17:06:37 -0800

    Stripe status/balance/payout route

--- routes/stripeRoute.js
+++ routes/stripeRoute.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const { createConnectStripe } = require("../controllers/paymentControllers");
+const {
+  createConnectStripe,
+  getAccountStatus,
+  getAccountBalance,
+  payoutSetting,
+} = require("../controllers/paymentControllers");
 // MIDDLEWARE
 const { requireSignin } = require("../middlewares/index");
 
 const router = express.Router();
 
 router.post("/create-connect-account", requireSignin, createConnectStripe);
+
+router.post("/get-account-status", requireSignin, getAccountStatus);
+
+router.post("/get-account-balance", requireSignin, getAccountBalance);
+
+router.post("/payout-setting", requireSignin, payoutSetting);
 
 module.exports = router;
