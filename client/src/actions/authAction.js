811e7ceeeca724fa8a21d7c5d1e145b0a7bf1697
FengDenny <fengdennyy@gmail.com>
2021-03-01 16:58:27 -0800

    update localStorage

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -4,3 +4,13 @@ export const register = async (user) =>
   await axios.post("/api/v1/register", user);
 
 export const login = async (user) => await axios.post("/api/v1/login", user);
+
+//update user in localStorage
+export const updateUserInLocalStorage = async (user, next) => {
+  if (window.localStorage.getItem("auth")) {
+    let auth = JSON.parse(localStorage.getItem("auth"));
+    auth.user = user;
+    localStorage.setItem("auth", JSON.stringify(auth));
+    next();
+  }
+};
